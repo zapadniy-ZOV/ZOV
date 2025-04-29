309d9ebad2e378a68349c2edac94a2a0d90b531f
ta4ilka69 <artem_balin_2004@mail.ru>
2025-04-29 23:13:19 +0300

    some security fix

--- src/main/java/itmo/rshd/config/CorsConfig.java
+++ src/main/java/itmo/rshd/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package itmo.rshd.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -7,37 +8,34 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
+
+    @Value("${allowed-origins}")
+    private String allowedOrigins;
 
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        
-        // Allow specific origins
-        config.setAllowedOrigins(Arrays.asList(
-            "http://localhost:5173",
-            "http://192.168.0.183:5173",
-            "http://172.18.80.1:5173",
-            "http://185.171.195.6:12348"
-        ));
-        
-        // Allow all headers
+
+        // Разделяем строки по запятой и убираем лишние пробелы
+        List<String> origins = Arrays.stream(allowedOrigins.split(","))
+                .map(String::trim)
+                .toList();
+        config.setAllowedOrigins(origins);
+
         config.addAllowedHeader("*");
-        
-        // Allow all HTTP methods
         config.addAllowedMethod("GET");
         config.addAllowedMethod("POST");
         config.addAllowedMethod("PUT");
         config.addAllowedMethod("DELETE");
         config.addAllowedMethod("OPTIONS");
-        
-        // Allow credentials
         config.setAllowCredentials(true);
-        
+
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
-} 
+}
